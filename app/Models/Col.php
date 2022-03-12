a8f3e0ee978937283f33aaf128b7403a9defded8
Nathan Wurtz <nathan.wurtz@incotec.fr>
2022-03-12 19:19:12 +0100

    Create/Move Models

---
+++ app/Models/Col.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Col extends Model
+{
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var boolean
+     */
+    public $timestamps = true;
+
+}
+
