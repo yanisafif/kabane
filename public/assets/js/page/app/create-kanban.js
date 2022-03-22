6b31786bbee15fad0d327eddfb272426bbb52ca2
Nathan Wurtz <nathan.wurtz01@gmail.com>
2022-03-22 10:46:12 +0100

    Fix create-kanban js script

--- public/assets/js/page/app/create-kanban.js
+++ public/assets/js/page/app/create-kanban.js
@@ -14,20 +14,20 @@ function createPicker(colorField) {
 }
 
 // Invitation
-let m = 1;
-const inviteContainer = document.getElementById("invite-field-container");
+let m = 1
+const inviteContainer = document.getElementById("invite-field-container")
 function addInvite() {
-    let element = document.createElement('div');
-    element.classList.add('small-group', 'mt-2');
-    element.id = 'invite-' + m;
+    let element = document.createElement('div')
+    element.classList.add('small-group', 'mt-2')
+    element.id = 'invite-' + m
 
     element.innerHTML =
             `
                 <input class="form-control" maxlength="50" name="invite[${m}]" type="text" required />
                 <img style="width: 20px; height: 20px" src="${window.location.protocol +'//'+ window.location.hostname}/assets/svg/close.svg" onclick="deleteInvite(${m})">
             `
-    inviteContainer.appendChild(element);
-    m++;
+    inviteContainer.appendChild(element)
+    m++
 }
 
 function deleteInvite(id) {
@@ -36,30 +36,31 @@ function deleteInvite(id) {
 
 
 // Column 
-let n = 2;
+let n = 2
 const colContainer = document.getElementById("col-fields-container");
 function addCol() {
-    let element = document.createElement('div');
-    element.classList.add('small-group', 'mt-1');
-    element.id = 'col-' + n;
+    let element = document.createElement('div')
+    element.classList.add('small-group', 'mt-1')
+    element.id = 'col-' + n
 
     element.innerHTML =
             `<div class="input-group">
                 <span class="input-group-text"> Name</span>
-                <input class="form-control" type="text" maxlength="50" name="colname[+ ${n} +]" required="" />
+                <input class="form-control" type="text" maxlength="50" name="colname[${n}]" required="" />
             </div>
             <div class="input-group color-field">
                 <span class="input-group-text">Color</span>
-                <input class="form-control" type="text" maxlength="9" name="colcolor['+ ${n} +']" required="" />
+                <input class="form-control" type="text" maxlength="9" name="colcolor[${n}]" required="" />
             </div>
-            <img style="width: 20px; height: 20px" src="${window.location.protocol +'//'+ window.location.hostname}/assets/svg/close.svg" onclick="deleteCol(${n})">`
-    colContainer.appendChild(element);
+            <img style="width: 20px; height: 20px" src="${window.location.protocol +'//'+ window.location.hostname}/assets/svg/close.svg" onclick="deleteCol(${n})">
+            `
+    colContainer.appendChild(element)
     onAddColumn(element)
-    n++;
+    n++
 }
 
 function onAddColumn(element) {
-    createPicker(element.querySelector('div.color-field'));
+    createPicker(element.querySelector('div.color-field'))
 }
 
 function deleteCol(id) {
